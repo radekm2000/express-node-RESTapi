caadcc75a396ade64b88e57e02209979efdaae48
Radek <104318242+radekm2000@users.noreply.github.com>
2023-05-17 16:35:15 +0200

    3rd commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,44 @@
 const {StatusCodes} = require('http-status-codes')
 const Product = require('../models/Product')
 
-const getAllProducts = async(req, res) => {
-    
-    const products = await Product.find({})
 
+const getAllProducts = async(req, res) => {
+    const {company, sort, select, name, page, numericFilters} = req.query
+    const queryObject = {}
+    let result =  Product.find(queryObject) // it returns a promise
+    
+    if(select) {
+        const selectList = select.split(',').join(" ")
+        result = Product.find({}).select(selectList)
+    }
+    if(numericFilters) {
+        const operatorMap = {
+            
+        }
+
+
+    }
+    if(name) {
+        const regex = new RegExp(name, 'i')
+        result = Product.find({name: { $regex: regex }})
+    }
+
+    if(company) {
+        queryObject.company = company
+    }
+
+    if(sort) {
+        const sortList = sort.split(',').join(" ") // syntax must be like(price name)
+        result.sort(sortList)
+    }
+    //pagination
+    
+    let limit = 7  || req.query.limit
+    const skip = (page - 1) * limit 
+    result.skip(skip).limit(limit)
+
+
+    const products = await result
     res.status(StatusCodes.OK).json({products, count: products.length})
 }
 const getSingleProduct = async(req, res) => {
@@ -42,6 +76,12 @@ const deleteProduct = async(req, res) => {
 
     return res.status(StatusCodes.OK).json({msg: "Product deleted"})
 }
+
+
+
+
+
+
 
 
 module.exports = {
